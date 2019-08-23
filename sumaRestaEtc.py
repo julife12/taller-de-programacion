b92f78747d3c550c8e0da7bf219a4383f0685a47
julife12 <julian.jaimes.2019@upb.edu.cp>
2019-08-23 11:10:53 -0500

    sexto ejercicio

---
+++ sumaRestaEtc.py
@@ -0,0 +1,8 @@
+a=int(input("escriba el primer numero: "))
+b=int(input("escriba el segundo numero: "))
+suma=a+b
+resta=a-b
+multiplicacion=a*b
+division=a/b
+residuo=a%b
+print("suma: ", suma, "multiplicacion: ", multiplicacion, "division: ", division, "residuo: ", residuo)
