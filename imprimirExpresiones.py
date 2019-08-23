0eae89f69a4dd2c4306dac1c39a32c5a189cbcf3
julife12 <julian.jaimes.2019@upb.edu.cp>
2019-08-23 11:03:29 -0500

    quinto ejercicio

---
+++ imprimirExpresiones.py
@@ -0,0 +1,5 @@
+uno=-5 + 8 * 6
+dos=(55+9) % 9
+tres=((20 + -3) * 5) / 8
+cuatro=( (((5 + 15) / 3) * (2 -8)) % 3)
+print("el resultado de la prima expresion es: ", uno, "el resultado de la segunda es: ", dos, "el resultado de la tercera es: ", tres, "el resultado de la cuarta expresion es: ", cuatro)
