b1289449232fa7e74dab60955b4c99b22647dceb
julife12 <julian.jaimes.2019@upb.edu.cp>
2019-08-26 19:12:58 -0500

    diez listo

---
+++ diez_areaPerimetroC.py
@@ -0,0 +1,5 @@
+radio=float(input("por favor digite el radio"))
+import math
+area= math.pi*(radio*radio)
+perimetro=(2*math.pi)*radio
+print("el area de el circulo es: ", area, "el perimetro es: ", perimetro)
